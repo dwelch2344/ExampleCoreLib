905c77afcbf69cd11c3d88c8935b5c355435131a
Dave Welch <dave.welch@mokinetworks.com>
2013-10-22 09:43:49 -0600

    Initial commit

---
+++ ExampleCoreLib/ExampleCoreLib.m
@@ -0,0 +1,13 @@
+//
+//  ExampleCoreLib.m
+//  ExampleCoreLib
+//
+//  Created by Dave Welch on 10/22/13.
+//  Copyright (c) 2013 David Welch. All rights reserved.
+//
+
+#import "ExampleCoreLib.h"
+
+@implementation ExampleCoreLib
+
+@end
